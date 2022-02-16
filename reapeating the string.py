260108a46d1c863cce023bf290a9537eb40f2121
ChandanaSree <99187261+chandanas-perspective-data@users.noreply.github.com>
2022-02-16 03:37:04 -0800

    Add files via upload

---
+++ reapeating the string.py
@@ -0,0 +1,16 @@
+def func():   ## The repetition operator is denoted by '*' symbol and is useful to repeat the string for several times.
+	str="python"
+	print(str*3)
+func()
+
+
+
+
+
+
+
+
+
+
+
+
