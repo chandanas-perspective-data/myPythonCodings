fcd9a31e607a3cdb32385e6add4ee6f15944dc94
ChandanaSree <99187261+chandanas-perspective-data@users.noreply.github.com>
2022-02-18 02:33:18 +0530

    Add files via upload

---
+++ Dictionaries.py
@@ -0,0 +1,13 @@
+# Dictionary represents a group of elements arranged in the form of key-value pairs.
+# In the Dictionary ,the first element is considered as 'key' and the immediate next element is taken as its 'value'.
+# All the key-value pairs in a dictionary are inserted in curly braces { }.
+## A Python program to create a dictionary with  employee details  and retrive the values upon giving the keys.
+ 
+
+dict = {'Name':'chandana','ID':101,'Salary':10,000}
+print('Name of the employee=',dict['Name'])
+print('ID number=',dict['ID'])
+print('Salary=',dict['Salary'])
+
+
+
