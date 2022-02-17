6f94bf827675e909bf34963473b5b318e58fd444
ChandanaSree <99187261+chandanas-perspective-data@users.noreply.github.com>
2022-02-18 02:32:32 +0530

    Add files via upload

---
+++ dict key values pair.py
@@ -0,0 +1,13 @@
+# A Python program  to retrive keys,values and key-value pairs from a dictionary
+ # dictionary methods 
+ # create a dictionary with employee details .
+  
+  dict = {'Name':'chandanasree','ID':201,'Salary':20.000}
+  print(dict) # print entire dictionary
+
+  print('keys in dict=',dict.keys()) # display only keys
+
+  print('value in dict=', dict.values()) # display only values
+  print("item in dist=",dict.item()) # display both key and value pairs as tuple
+  
+
